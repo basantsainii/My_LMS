57db6b35c7c3189eaf1a1d23c6bfaf536f166159
basantsainii <bsntsainii@gmail.com>
2025-04-06 10:59:15 +0530

    css update 1

--- src/Lsider.jsx
+++ src/Lsider.jsx
@@ -13,26 +13,28 @@ function Lsider() {
   // const Activate_Bgclr = (x) => {
   //   setBgclr(x)
   // }
+  const Active = "bg-blue-700 text-white rounded-lg p-5"
+  const Pending = "p-5 hover:bg-blue-500 hover:text-white rounded-lg font-medium hover:shadow-2xl my-1"
 
   return (
     <>
       <div className='h-[98%]  relative left-2 top-2  p-2 bg-white shadow-sm flex flex-col justify-start align-middle '>
-        <NavLink to="/" className={({ isActive, isPending }) => isPending ? " " : isActive ? "bg-blue-800 text-white rounded-lg p-5" : "p-5 hover:bg-blue-800 hover:text-white rounded-lg font-medium hover:shadow-2xl"}>
+        <NavLink to="/" className={({ isActive, isPending }) => isPending ? " " : isActive ? `${Active}` : `${Pending}`}>
           <div>
             Module
           </div>
         </NavLink>
-        <NavLink to="/Card" className={({ isActive, isPending }) => isPending ? " " : isActive ? "bg-blue-800 text-white rounded-lg p-5" : " p-5 hover:bg-blue-800 hover:text-white rounded-lg font-medium hover:shadow-2xl"}>
+        <NavLink to="/Card" className={({ isActive, isPending }) => isPending ? " " : isActive ? `${Active}` : `${Pending}`}>
           <div  >
             Batch
           </div>
         </NavLink>
-        <NavLink to="/Test_Module" className={({ isActive, isPending }) => isPending ? " " : isActive ? "bg-blue-800 text-white rounded-lg p-5" : "p-5 hover:bg-blue-800 hover:text-white rounded-lg font-medium hover:shadow-2xl"}>
+        <NavLink to="/Test_Module" className={({ isActive, isPending }) => isPending ? " " : isActive ? `${Active}` : `${Pending}`}>
           <div >
             Test Module
           </div>
         </NavLink>
-        <NavLink to="/Resources" className={({ isActive, isPending }) => isPending ? " " : isActive ? "bg-blue-800 text-white rounded-lg p-5" : "p-5 hover:bg-blue-800 hover:text-white rounded-lg font-medium hover:shadow-2xl"}>
+        <NavLink to="/Resources" className={({ isActive, isPending }) => isPending ? " " : isActive ? `${Active}` : `${Pending}`}>
           <div>
             Resources
           </div>
