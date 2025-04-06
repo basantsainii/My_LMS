3d21345c046888160130f532851b671a89e4cd71
basantsainii <bsntsainii@gmail.com>
2025-04-06 12:59:18 +0530

    css update 4

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import App from './App.jsx'
 import './All_css/index.css'
 import { CurrentContextProvider } from './Context_API/CurrentContext.jsx'
 import { SearchContextProvider } from './Context_API/SearchContext.jsx'
-import { HumBurgerContextProvider } from './Context_API/responsiveContext.jsx'
+import { HumBurgerContextProvider } from './Context_API/ResponsiveContext.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <CurrentContextProvider >
         <SearchContextProvider>
